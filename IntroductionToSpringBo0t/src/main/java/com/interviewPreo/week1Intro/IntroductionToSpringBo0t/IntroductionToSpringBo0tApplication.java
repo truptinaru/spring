160e3ea677cadc49627c50ba39d3453118b91a22
TruptiNarvekar <truptinarvekar631@gmail.com>
2025-02-06 10:09:58 +0530

    setting up code

---
+++ IntroductionToSpringBo0t/src/main/java/com/interviewPreo/week1Intro/IntroductionToSpringBo0t/IntroductionToSpringBo0tApplication.java
@@ -0,0 +1,13 @@
+package com.interviewPreo.week1Intro.IntroductionToSpringBo0t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBo0tApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBo0tApplication.class, args);
+	}
+
+}
---
+++ IntroductionToSpringBo0t/src/test/java/com/interviewPreo/week1Intro/IntroductionToSpringBo0t/IntroductionToSpringBo0tApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewPreo.week1Intro.IntroductionToSpringBo0t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBo0tApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
